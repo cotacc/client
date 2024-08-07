0be68f9323d6c2c5a3edcd6ae4be395e317a6b6e
cotacc <cotmemodistribution@gmail.com>
2024-08-07 15:28:42 +0800

    Update usercalendar.jsx

--- client/src/pages/user/usercalendar.jsx
+++ client/src/pages/user/usercalendar.jsx
@@ -22,7 +22,7 @@ const Usercalendar = () => {
   useEffect(() => {
     const fetchUserAndEvents = async () => {
       try {
-        const response = await axios.get('https://server-gzmw.onrender.com/api/memoreceivesendthismonth', {
+        const response = await axios.get('https://client-server-f5nt.onrender.com/api/memoreceivesendthismonth', {
           params: { token },
           headers: { Authorization: `Bearer ${token}` }
         });
@@ -54,7 +54,7 @@ const Usercalendar = () => {
     const formattedDate = `${year}-${month}-${day}`;
 
     try {
-      const response = await axios.post("https://server-gzmw.onrender.com/api/memo/send-and-recieve", {
+      const response = await axios.post("https://client-server-f5nt.onrender.com/api/memo/send-and-recieve", {
         date: formattedDate,
         token
       }, {
@@ -83,7 +83,7 @@ const Usercalendar = () => {
   const handleEventClick = (event) => {
     if (event.type === 'Received') {
       axios.post(
-        'https://server-gzmw.onrender.com/api/memo/read',
+        'https://client-server-f5nt.onrender.com/api/memo/read',
         {
           token,
           memoId:event.memoid,
